61bae8dea80e07cbdc280f076441dec21e459946
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-18 21:46:06 -0400

    Implement table displaying containers

--- src/Containers.tsx
+++ src/Containers.tsx
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
+interface Container {
+  containerId: string,
+  containerShortId: string,
+  containerImage: string,
+  containerName: string,
+  vscodeUri: string,
+}
 
 function Containers() {
+  const [containers, setContainers] = useState<Container[]>([]);
+
+  // Fetch info of all containers
+  useEffect(() => {
+    const fetchContainers = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/fetch/containers/info/all');
+        const responseJson = await response.json();
+        console.log(responseJson);
+        setContainers(responseJson.containers);
+      } catch (err: unknown) {
+        console.error(err)
+      }
+    };
+
+    fetchContainers();
+  }, []);
+
   return (
-    <div css={{ display: 'flex' }}>
-      Containers
-    </div>
+    <TableContainer>
+      <Table aria-label="container table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Short ID</TableCell>
+            <TableCell>Container Name</TableCell>
+            <TableCell>Base Image</TableCell>
+            <TableCell>VsCode URI</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {containers.map((c) => (
+            <TableRow
+              key={c.containerId}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">{c.containerShortId}</TableCell>
+              <TableCell>{c.containerName}</TableCell>
+              <TableCell>{c.containerImage}</TableCell>
+              <TableCell>{c.vscodeUri}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
