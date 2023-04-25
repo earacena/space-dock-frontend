e354692241acbd0dae4802d9df8a24c79e0d7139
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-25 00:31:14 -0400

    Implement elevating app bar when scrolling

--- src/features/Sidebar/Sidebar.tsx
+++ src/features/Sidebar/Sidebar.tsx
@@ -1,12 +1,16 @@
 import React, { Fragment, KeyboardEvent, MouseEvent, useState } from 'react';
 import { useNavigate } from 'react-router';
-import { AppBar, Box, CssBaseline, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, CssBaseline, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography, useScrollTrigger } from '@mui/material';
 import LayersIcon from '@mui/icons-material/Layers';
 import DeveloperBoardIcon from '@mui/icons-material/DeveloperBoard';
 import HomeIcon from '@mui/icons-material/Home';
 import { Menu } from '@mui/icons-material';
 
 const drawerWidth = 300;
+
+interface ElevationScrollProps {
+  children: React.ReactElement,
+}
 
 function Sidebar() {
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
@@ -17,6 +21,19 @@ function Sidebar() {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+
+  const ElevationScroll: React.FC<ElevationScrollProps> = (props) => {
+    const { children } = props;
+
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
 
   const drawerContents = (
     <List sx={{ width: '300px', maxWidth: '360px', padding: 0 }}>
@@ -70,29 +87,32 @@ function Sidebar() {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { md: `calc(100% - ${drawerWidth}px)`},
-          ml: { md: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar sx={{ display: 'flex', flexDirection: 'row' }}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { md: 'none' } }}
-          >
-            <Menu />
-          </IconButton>
-          <span css={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', flexGrow: 1 }}>
-            <span role="img" aria-label="satalitte" css={{ fontSize: '40px', paddingRight: '5px' }}>🛰</span>
-            <Typography variant="h4" component="h1" sx={{ fontFamily: "Abel" }}>Space Dock</Typography>
-          </span>
-        </Toolbar>
-      </AppBar>
+      <ElevationScroll>
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { md: `calc(100% - ${drawerWidth}px)`},
+            ml: { md: `${drawerWidth}px` },
+          }}
+        >
+          <Toolbar sx={{ display: 'flex', flexDirection: 'row' }}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { md: 'none' } }}
+            >
+              <Menu />
+            </IconButton>
+            <span css={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', flexGrow: 1 }}>
+              <span role="img" aria-label="satalitte" css={{ fontSize: '40px', paddingRight: '5px' }}>🛰</span>
+              <Typography variant="h4" component="h1" sx={{ fontFamily: "Abel" }}>Space Dock</Typography>
+            </span>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+
       <Box
         component="nav"
         sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
@@ -128,7 +148,6 @@ function Sidebar() {
         >
           {drawerContents}
         </Drawer>
-
       </Box>
     </Box>
   );
