664a91c0e6dfd2b936d2d2172d0ca55b3d4de720
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-25 23:56:42 -0400

    Change component from modal to dialog

---
+++ src/features/Images/ImageFormDialog.tsx
@@ -0,0 +1,32 @@
+import { Box, Dialog, DialogContent, DialogTitle, useMediaQuery, useTheme } from '@mui/material';
+import React, { SetStateAction, useState } from 'react';
+import ImageForm from './ImageForm';
+
+interface ImageFormDialogProps {
+  open: boolean,
+  setOpen: (value: SetStateAction<boolean>) => void,
+}
+
+function ImageFormDialog({ open, setOpen }: ImageFormDialogProps) {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+  return (
+    <Dialog
+      fullScreen={fullScreen}
+      open={open}
+      onClose={() => setOpen(false)}
+      maxWidth="sm"
+      fullWidth
+    >
+      <DialogTitle id="image form dialog">
+        Build an image
+      </DialogTitle>
+      <DialogContent>
+        <ImageForm imageFormDialogOpen={open} setImageFormDialogOpen={setOpen} />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ImageFormDialog;
