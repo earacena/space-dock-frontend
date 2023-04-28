418cf254d2cb1e3fc9f54cd721631a1b284d2492
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-28 01:19:46 -0400

    Implement function for creating containers

--- src/features/Containers/api/container.service.ts
+++ src/features/Containers/api/container.service.ts
@@ -1,5 +1,18 @@
 const backendUrl = "http://localhost:5000";
 
+const create = async (imageId: string) => {
+  const response = await fetch(`${backendUrl}/create/container/${imageId}`, {
+    method: 'POST'
+  });
+
+  const responseJson = await response.json();
+  if (responseJson.error) {
+    console.error(responseJson.error);
+  }
+
+  return responseJson;
+};
+
 const fetchAll = async () => {
   const response = await fetch(`${backendUrl}/fetch/container/info/all`);
   const responseJson = await response.json();
@@ -14,6 +27,7 @@ const fetchAll = async () => {
 
 const actions = {
   fetchAll,
+  create,
 };
 
 export default actions;
