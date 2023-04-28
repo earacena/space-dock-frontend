3c5b033de8379b5d41ccc0695d5235ba4331377a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-04-28 01:52:55 -0400

    Add loading icon on first connection to backend

--- src/features/Containers/ContainerList.tsx
+++ src/features/Containers/ContainerList.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { List } from '@mui/material';
+import { Box, CircularProgress, List, Typography } from '@mui/material';
 import ContainerListItem from './ContainerListItem';
 import type { Container as ContainerType } from './container.types';
 import containerService from './api/container.service';
 
 function ContainerList() {
-  const [containers, setContainers] = useState<ContainerType[]>([]);
+  const [containers, setContainers] = useState<ContainerType[] | undefined>(undefined);
 
   // Fetch info of all containers
   useEffect(() => {
@@ -21,12 +21,21 @@ function ContainerList() {
     fetchContainers();
   }, []);
 
+  const areThereContainers: boolean = containers !== undefined && containers.length !== 0;
+
   return (
-    <List>
-      {containers.map((c) => (
-        <ContainerListItem key={c.containerId} container={c} />
-      ))}
-    </List>
+    <Box>
+      <List sx={{ display: areThereContainers ? '' : 'none' }}>
+        {containers?.map((c) => (
+          <ContainerListItem key={c.containerId} container={c} />
+        ))}
+      </List>
+
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', my: '20px' }}>
+        {containers === undefined && <CircularProgress sx={{ margin: '10px' }} />}
+        {containers?.length === 0 && <Typography>You have not created any containers. Go to the 'Images' tab to launch a container from an image.</Typography>}
+      </Box>
+    </Box>
   );
 }
 
